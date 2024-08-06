8dbac59951b92a55a35d12c7317a5177370f1be3
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-08-06 07:32:45 +0530

    Added HTTP Apis using gin gonic

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/ashiqYousuf/simple_bank/api"
+	db "github.com/ashiqYousuf/simple_bank/db/sqlc"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	log.Fatal(server.Start(serverAddress))
+}
