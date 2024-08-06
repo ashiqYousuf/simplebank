6da3a7a0db6ad40b1e8110b2d21edeec02d31dc0
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-08-06 07:56:46 +0530

    Use viper for loading env vars

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/ashiqYousuf/simple_bank/api"
 	db "github.com/ashiqYousuf/simple_bank/db/sqlc"
+	"github.com/ashiqYousuf/simple_bank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,5 +25,5 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	log.Fatal(server.Start(serverAddress))
+	log.Fatal(server.Start(config.ServerAddress))
 }
