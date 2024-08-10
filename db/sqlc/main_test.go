ff544c6cc326dbf5d8614649746e2d4da8636d25
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-08-10 08:12:38 +0530

    UNIT testing accounts CRUD

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+/*
+TestMain function is the main entry point of all unit tests
+inside one specific golang package
+*/
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
